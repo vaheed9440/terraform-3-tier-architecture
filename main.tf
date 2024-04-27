e9f82036e46b0c2d23c02f7c6a8c3847fde58b3a
vaheed9440 <124774319+vaheed9440@users.noreply.github.com>
2024-04-27 16:46:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -134,7 +134,7 @@ resource "aws_route_table_association" "b" {
 
 #Create EC2 Instance
 resource "aws_instance" "webserver1" {
-  ami                    = "ami-02d7fd1c2af6eead0"
+  ami                    = "ami-0a1179631ec8933d7"
   instance_type          = "t2.micro"
   availability_zone      = "us-east-1a"
   key_name               = "devops"
@@ -148,7 +148,7 @@ resource "aws_instance" "webserver1" {
 }
 
 resource "aws_instance" "webserver2" {
-  ami                    = "ami-02d7fd1c2af6eead0"
+  ami                    = "ami-0a1179631ec8933d7"
   instance_type          = "t2.micro"
   availability_zone      = "us-east-1b"
   key_name               = "devops"
@@ -163,7 +163,7 @@ resource "aws_instance" "webserver2" {
 
 #Create EC2 Instance
 resource "aws_instance" "appserver1" {
-  ami                    = "ami-02d7fd1c2af6eead0"
+  ami                    = "ami-0a1179631ec8933d7"
   instance_type          = "t2.micro"
   availability_zone      = "us-east-1a"
   key_name               = "devops"
@@ -175,7 +175,7 @@ resource "aws_instance" "appserver1" {
 }
 
 resource "aws_instance" "appserver2" {
-  ami                    = "ami-02d7fd1c2af6eead0"
+  ami                    = "ami-0a1179631ec8933d7"
   instance_type          = "t2.micro"
   availability_zone      = "us-east-1b"
   key_name               = "devops"
